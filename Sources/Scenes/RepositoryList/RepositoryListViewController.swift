ede8d4da599be26c173c73194987984424458716
mauriciobalenamazzocco <mauriciomazzocco@gmail.com>
2020-07-08 18:27:31 -0300

    Removed unused Router

--- Sources/Scenes/RepositoryList/RepositoryListViewController.swift
+++ Sources/Scenes/RepositoryList/RepositoryListViewController.swift
@@ -22,8 +22,6 @@ protocol RepositoryListDisplayLogic: class {
 class RepositoryListViewController: UIViewController, RepositoryListDisplayLogic {
 
     var interactor: RepositoryListBusinessLogic?
-    var router: (NSObjectProtocol & RepositoryListRoutingLogic & RepositoryListDataPassing)?
-
     // MARK: Object lifecycle
 
     override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
@@ -42,24 +40,9 @@ class RepositoryListViewController: UIViewController, RepositoryListDisplayLogic
         let viewController = self
         let interactor = RepositoryListInteractor()
         let presenter = RepositoryListPresenter()
-        let router = RepositoryListRouter()
         viewController.interactor = interactor
-        viewController.router = router
         interactor.presenter = presenter
         presenter.viewController = viewController
-        router.viewController = viewController
-        router.dataStore = interactor
-    }
-
-    // MARK: Routing
-
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        if let scene = segue.identifier {
-            let selector = NSSelectorFromString("routeTo\(scene)WithSegue:")
-            if let router = router, router.responds(to: selector) {
-                router.perform(selector, with: segue)
-            }
-        }
     }
 
     // MARK: Views
