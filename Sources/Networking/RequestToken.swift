892c159d527b1c7c7ebc5262401b02c38b50f46b
mauriciobalenamazzocco <mauriciomazzocco@gmail.com>
2020-07-08 17:46:00 -0300

    fix Spaces

--- Sources/Networking/RequestToken.swift
+++ Sources/Networking/RequestToken.swift
@@ -9,12 +9,12 @@
 import Foundation
 class RequestToken {
     private weak var task: URLSessionDataTask?
+
     init(task: URLSessionDataTask?) {
         self.task = task
 
     }
     func cancel() {
         task?.cancel()
-
     }
 }
