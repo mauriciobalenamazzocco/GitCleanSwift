08a181823fcff844e2b6b2f86dda5ea21d2cdcff
mauriciobalenamazzocco <mauriciomazzocco@gmail.com>
2020-07-10 11:57:45 -0300

    test test_CellPrepareForReuse

--- GitCleanSwiftTests/Scene/RepositoryList/RepositoryListViewControllerTests.swift
+++ GitCleanSwiftTests/Scene/RepositoryList/RepositoryListViewControllerTests.swift
@@ -213,4 +213,31 @@ class RepositoryListViewControllerTests: XCTestCase
 
         XCTFail()
     }
+
+    func test_CellPrepareForReuse()
+    {
+        // Given
+        let tableView = repositoryListViewController.tableView
+        let displayRepository = RepositoryList.FetchRepositories.ViewModel.DisplayedRepository(repoName: "repoName", userAvatarPath: "avatarName", repoStarsCount: "repoStarCount", userProfilePath: "userProfilePath")
+        let testDisplayedRepositories = [displayRepository]
+        repositoryListViewController.displayedRepositories = testDisplayedRepositories
+
+        // When
+        let indexPath = IndexPath(row: 0, section: 0)
+
+        // Then
+        if let cell = repositoryListViewController.tableView(tableView, cellForRowAt: indexPath) as? RepositoryCell {
+            XCTAssertEqual(cell.repositoryNameLabel.text, "repoName")
+            XCTAssertEqual(cell.starsLabel.text, "repoStarCount")
+
+            cell.prepareForReuse()
+            XCTAssertEqual(cell.repositoryNameLabel.text, "")
+            XCTAssertEqual(cell.avatarImageView.image, nil)
+            XCTAssertEqual(cell.starsLabel.text, "")
+            XCTAssertEqual(cell.userNameLabel.text, "")
+            return
+        }
+
+        XCTFail()
+    }
 }
